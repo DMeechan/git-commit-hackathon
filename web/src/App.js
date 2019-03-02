7d628d758b1abdc2f1a3709d9bcd81d923fc81a5
moxis <mail@koxi.pw>
2019-03-02 17:48:27 +0000

    sockets test

--- web/src/App.js
+++ web/src/App.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Form, Select, InputNumber, DatePicker, Switch, Slider, Button, Card, Row, Col, Steps } from 'antd';
 import './App.css';
+import socketIOClient from "socket.io-client";
 
 const { Option } = Select;
 const Step = Steps.Step;
 
-const App = () => (
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      response: false,
+      endpoint: "http://127.0.0.1:8080",
+    }
+  }
+
+  componentDidMount() {
+    const { endpoint } = this.state;
+    const socket = socketIOClient(endpoint);
+    socket.on("join", data => this.setState({ response: data }));
+  }
+  render() {
+    const { response } = this.state;
+    return (
+      <div style={{ textAlign: "center" }}>
+        {response
+          ? <p>
+              The temperature in Florence is: {response} °F
+            </p>
+          : <p>Loading...</p>}
+      </div>
+    );
+  }
+}
+
+const AppOrg = () => (
   <div className="App">
   <Card title={<h1 style={{ marginBottom: '0px' }}><b>MT1003 Module Feedback</b></h1>}>
   <p>Please describe your experience.</p>
