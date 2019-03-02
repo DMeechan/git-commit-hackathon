5f3be1071b93255466e8cc518316d853326f5c3d
Daniel Meechan <dm280@st-andrews.ac.uk>
2019-03-02 15:22:49 +0000

    Implemented basic speech to text

--- server/integrations/speech-to-text.js
+++ server/integrations/speech-to-text.js
@@ -1,23 +1,34 @@
-const fs = require('fs');
-const speech = require('@google-cloud/speech');
+async function main() {
+    const fs = require('fs');
+    const speech = require('@google-cloud/speech');
 
-const client = new speech.SpeechClient();
+    const client = new speech.SpeechClient();
 
-const fileName = '../files/audio.mp3';
+    const fileName = 'files/obama.mp3';
 
-const file = fs.readFileSync(fileName);
-const audioBytes = file.toString(base64);
+    const file = fs.readFileSync(fileName);
+    const audioBytes = file.toString('base64');
 
-const audio = {
-    content: audioBytes,
-};
+    const audio = {
+        content: audioBytes,
+    };
 
-const config = {
-    encoding: 'LINEAR16',
-    sampleRateHertz: 16000,
-    languageCode: 'en-US',
-};
+    const config = {
+        encoding: 'LINEAR16',
+        sampleRateHertz: 16000,
+        languageCode: 'en-US',
+    };
 
-const request = {
-    audio, config
-};
+    const request = {
+        audio, config
+    };
+
+    // Detects speech in the audio file
+    const [response] = await client.recognize(request);
+    const transcription = response.results
+        .map(result => result.alternatives[0].transcript)
+        .join('\n');
+    console.log(`Transcription: ${transcription}`);
+}
+
+main().catch(console.error);
